45a7af2e45d9acecb75fe681cc31790cb7ccd52d
root <root@DESKTOP-Q920NU0.localdomain>
2022-05-16 15:08:56 +0430

    update login nav

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -6,7 +6,7 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mb-2 mb-lg-0">
-      @guest
+      <!-- @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -18,7 +18,7 @@
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
-        @else
+        @else -->
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">خانه</a>
         </li>
@@ -85,7 +85,7 @@
             </li>
           </ul>
         </li>
-        @endguest
+        <!-- @endguest -->
         <!-- <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
         </li>
