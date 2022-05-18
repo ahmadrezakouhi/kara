09fc7dc10c7ed6731b0d9a849f7f1cf497fa952d
root <root@DESKTOP-Q920NU0.localdomain>
2022-05-18 08:48:52 +0430

    change max to kara

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-themed">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">کاراله - مدیریت بهره وری پروژه ها</a>
+    <a class="navbar-brand" href="#">کارا - مدیریت بهره وری پروژه ها</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
