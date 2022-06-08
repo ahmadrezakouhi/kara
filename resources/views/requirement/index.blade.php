f4c23dc082e5e0063603c495dde83ebc1dc67a66
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-06-08 09:40:30 +0430

    add icons to buttons

--- resources/views/requirement/index.blade.php
+++ resources/views/requirement/index.blade.php
@@ -126,14 +126,23 @@
                     data: null,
                     title: "مدیریت",
                     className: "center",
-                    defaultContent: "<button class='btn btn-danger delete'>delete</button>" +
-                        "<button class='btn btn-warning edit'>edit</button>"
+                    defaultContent: "<button class='btn btn-danger delete'>" +
+                        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">' +
+                        '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>' +
+                        '<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>' +
+                        '</svg>' +
+                        "</button>" +
+                        "<button class='btn btn-warning edit'>" +
+                        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">' +
+                        '<path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>' +
+                        '</svg>' +
+                        "</button>"
                 }
 
             ];
             var table =
                 datatable('#tbl_requirements',
-                    '{{ route("projects.requirements.index", $project->id) }}',
+                    '{{ route('projects.requirements.index', $project->id) }}',
                     columns);
 
 
@@ -141,13 +150,13 @@
             $('#create_update').submit(function(event) {
                 event.preventDefault();
                 submit_form(this, clickButtonID,
-                    '{{ route("projects.requirements.store") }}', '#add_requirements', table);
+                    '{{ route('projects.requirements.store') }}', '#add_requirements', table);
             })
 
 
             $(document).on('click', '.delete', function(event) {
                 var id = $(this).attr('data-id');
-                var url = "{{ route('projects.requirements.store') }}" + '/'+ id ;
+                var url = "{{ route('projects.requirements.store') }}" + '/' + id;
                 ajaxfunc(url, 'DELETE', '');
                 table.ajax.reload();
 
@@ -168,11 +177,12 @@
                 //     },
 
                 // })
-             var  res = ajaxfunc('{{ route("projects.requirements.store") }}'+'/'+clickButtonID,'GET','')
-                    //  $('#title').val(res.title);
-                    //     $('#description').val(res.description);
-                    //     $('#add_requirements').modal('show');
-                    // console.log(res)
+                var res = ajaxfunc('{{ route('projects.requirements.store') }}' + '/' + clickButtonID,
+                    'GET', '')
+                //  $('#title').val(res.title);
+                //     $('#description').val(res.description);
+                //     $('#add_requirements').modal('show');
+                // console.log(res)
             });
 
             $('#create_button').click(function() {
