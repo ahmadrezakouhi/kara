5848234d6230b93aebdca890d95b288f682e6716
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-06-08 15:48:11 +0430

    implement relationship between Sprint and Phase in Sprint model

--- app/Models/Sprint.php
+++ app/Models/Sprint.php
@@ -13,4 +13,8 @@ class Sprint extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function phase(){
+        return $this->belongsTo(Phase::class);
+    }
 }
