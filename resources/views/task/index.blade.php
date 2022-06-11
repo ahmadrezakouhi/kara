7d95e27e1fe7fb41e2048ee33136d914491f9380
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-06-11 15:44:00 +0430

    implement ui in task/index.blade.php

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -5,9 +5,9 @@
     <div class="container">
         <div class="card shadow-sm mt-5">
             <div class="card-header d-flex justify-content-between">
-                <h2>اسپرینت های پروژه {{ $phase->title }}</h2>
+                <h2>تسک های اسپرینت {{ $sprint->title }}</h2>
                 <button class="btn btn-success" id="create_button"> افزودن
-                    اسپرینت</button>
+                    تسک</button>
             </div>
             <div class="card-body">
                 <div class="row pt-3">
@@ -47,8 +47,7 @@
                         <th>عنوان</th>
                         <th>توضیحات</th>
                         <th>مدت زمان انجام</th>
-                        <th>تاریخ شروع</th>
-                        <th>تاریخ پایان</th>
+                        <th>دسته بندی</th>
                         <th></th>
                         {{-- <th>تاریخ ثبت</th>
                         <th>مدیریت</th> --}}
@@ -69,11 +68,11 @@
 
                 <!-- Modal Header -->
                 <div class="modal-header">
-                    <h4 class="modal-title" id="modal_title">افزودن اسپرینت ها</h4>
+                    <h4 class="modal-title" id="modal_title">افزودن تسک ها</h4>
                     <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                 </div>
                 <form action="" action="post" id="create_update">
-                    <input type="hidden" name="phase_id" value="{{ $phase->id }}">
+                    <input type="hidden" name="sprint_id" value="{{ $sprint->id }}">
                     <input type="hidden" name="requirement_id">
                     <!-- Modal body -->
                     <div class="modal-body">
@@ -81,25 +80,26 @@
                             <label for="title" class="form-label">عنوان</label>
                             <input type="text" class="form-control" id="title" name="title">
                         </div>
+                        <div class="mb-3 mt3">
+                            <label for="category" class="form-label">دسته بندی</label>
+                            <select class="form-select" id="category" aria-label="Default select example" name="category">
+                                @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                @endforeach
+
+                              </select>
+                        </div>
                         <div class="mb-3 mt-3">
                             <label for="description" class="form-label">توضیحات</label>
                             <textarea name="description" id="description" cols="30" rows="10" class="form-control"></textarea>
                         </div>
                         <div class="mb-3 mt-3">
-                            <div class="row">
-                                <div class="col-md-4">
+
+
                                     <label for="duration" class="form-label">مدت زمان انجام</label>
                                     <input type="text" class="form-control" id="duration" name="duration">
-                                </div>
-                                <div class="col-md-4">
-                                    <label for="start_date" class="form-label">زمان شروع</label>
-                                    <input type="text" class="form-control" id="start_date" name="start_date">
-                                </div>
-                                <div class="col-md-4">
-                                    <label for="end_date" class="form-label">زمان پایان</label>
-                                    <input type="text" class="form-control" id="end_date" name="end_date">
-                                </div>
-                            </div>
+
+
                         </div>
                     </div>
 
@@ -147,25 +147,9 @@
                     data: 'duration'
                 },
                 {
-                    data: null,
-                    render: function(data, type, row) {
-                        return moment(data["start_date"], 'YYYY-M-D HH:mm:ss').format(
-                            'jYYYY/jMM/jDD');
-
-                    },
-                    "visible": true,
-                    responsivePriority: 2,
+                    data: 'category.name'
                 },
-                {
-                    data: null,
-                    render: function(data, type, row) {
-                        return moment(data["end_date"], 'YYYY-M-D HH:mm:ss').format(
-                            'jYYYY/jMM/jDD');
 
-                    },
-                    "visible": true,
-                    responsivePriority: 2,
-                },
                 {
                     responsivePriority: 0,
                     data: null,
@@ -181,28 +165,20 @@
                         '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">' +
                         '<path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>' +
                         '</svg>' +
-                        "</button>" +
-                        "</button>" +
-                        "<button class='btn btn-info sprints' >" +
-                        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list-stars" viewBox="0 0 16 16">' +
-                        '<path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z"/>' +
-                        '<path d="M2.242 2.194a.27.27 0 0 1 .516 0l.162.53c.035.115.14.194.258.194h.551c.259 0 .37.333.164.493l-.468.363a.277.277 0 0 0-.094.3l.173.569c.078.256-.213.462-.423.3l-.417-.324a.267.267 0 0 0-.328 0l-.417.323c-.21.163-.5-.043-.423-.299l.173-.57a.277.277 0 0 0-.094-.299l-.468-.363c-.206-.16-.095-.493.164-.493h.55a.271.271 0 0 0 .259-.194l.162-.53zm0 4a.27.27 0 0 1 .516 0l.162.53c.035.115.14.194.258.194h.551c.259 0 .37.333.164.493l-.468.363a.277.277 0 0 0-.094.3l.173.569c.078.255-.213.462-.423.3l-.417-.324a.267.267 0 0 0-.328 0l-.417.323c-.21.163-.5-.043-.423-.299l.173-.57a.277.277 0 0 0-.094-.299l-.468-.363c-.206-.16-.095-.493.164-.493h.55a.271.271 0 0 0 .259-.194l.162-.53zm0 4a.27.27 0 0 1 .516 0l.162.53c.035.115.14.194.258.194h.551c.259 0 .37.333.164.493l-.468.363a.277.277 0 0 0-.094.3l.173.569c.078.255-.213.462-.423.3l-.417-.324a.267.267 0 0 0-.328 0l-.417.323c-.21.163-.5-.043-.423-.299l.173-.57a.277.277 0 0 0-.094-.299l-.468-.363c-.206-.16-.095-.493.164-.493h.55a.271.271 0 0 0 .259-.194l.162-.53z"/>' +
-                        '</svg>' +
                         "</button>"
                 }
 
             ];
             var table =
                 datatable('#tbl_requirements',
-                    '{{ route("phases.sprints.index", $phase->id) }}',
+                    '{{ route("sprints.tasks.index", $sprint->id) }}',
                     columns);
 
 
 
             $('#create_update').submit(function(event) {
                 event.preventDefault();
-                submit_form('#create_update', clickButtonID,
-                        '{{ route("phases.sprints.store") }}', '#add_requirements', table)
+                submit_form('#create_update', clickButtonID,'{{ route("sprints.tasks.store") }}', '#add_requirements', table)
                     .then(function(res) {
                         toastr['success'](res.message);
                         table.ajax.reload();
@@ -214,7 +190,7 @@
 
             $(document).on('click', '.delete', function(event) {
                 var id = $(this).attr('data-id');
-                var url = "{{ route('phases.sprints.store') }}" + '/' + id;
+                var url = "{{ route('sprints.tasks.store') }}" + '/' + id;
                 // ajaxfunc(url, 'DELETE', '').then(function(res) {
                 //     toastr['success'](res.message)
                 // })
@@ -248,7 +224,7 @@
 
 
 
-                ajaxfunc('{{ route('phases.sprints.store') }}' + '/' + clickButtonID,
+                ajaxfunc("{{ route('sprints.tasks.store') }}" + '/' + clickButtonID,
                         'GET', '').then(function(res) {
                         if (res.message) {
                             toastr['success'](res.message)
@@ -257,8 +233,7 @@
                         $('#title').val(res['title']);
                         $('#description').val(res.description);
                         $('#duration').val(res.duration);
-                        $('#start_date').val(covertJalaliToGregorian(res.start_date));
-                        $('#end_date').val(covertJalaliToGregorian(res.end_date));
+                        $('#category').val(res.category_id);
                         $('#add_requirements').modal('show');
                     })
                     .catch(function(res) {
