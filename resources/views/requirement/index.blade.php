fa0db7ea4e46e79aa6d023ae10000d0c2d1e53be
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-06-13 09:14:45 +0430

    create ui for requirement/index.blade.php

--- resources/views/requirement/index.blade.php
+++ resources/views/requirement/index.blade.php
@@ -46,6 +46,7 @@
                         <th></th>
                         <th>عنوان</th>
                         <th>توضیحات</th>
+                        <th>فاز</th>
                         <th>تاریخ ثبت</th>
                         <th>مدیریت</th>
 
@@ -92,7 +93,38 @@
             </div>
         </div>
     </div>
+    <div class="modal fade" id="add_phase">
+        <div class="modal-dialog">
+            <div class="modal-content">
 
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title" id="modal_title">تعیین فاز نیازمندی ها</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+                <form action="" action="post" id="add_requrements_phase">
+                    {{-- <input type="hidden" name="project_id" value="{{ $project->id }}"> --}}
+
+                    <!-- Modal body -->
+                    <div class="modal-body">
+                        @foreach ($phases as $phase)
+                            <div class="form-check">
+                                <input type="radio" class="form-check-input" id="phase{{ $phase->id }}" name="phase_id"
+                                    value="{{ $phase->id }}">{{ $phase->title }}
+                                <label class="form-check-label" for="phase{{ $phase->id }}"></label>
+                            </div>
+                        @endforeach
+                    </div>
+
+                    <!-- Modal footer -->
+                    <div class="modal-footer">
+                        <button type="submit" id="submit-form" class="btn btn-success"
+                            data-bs-dismiss="modal">افزودن</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
 @endsection
 @section('scripts')
@@ -106,8 +138,7 @@
     <script>
         $(document).ready(function() {
             var clickButtonID;
-            var columns = [
-                {
+            var columns = [{
                     title: 'ردیف',
                     "defaultContent": "-",
                 },
@@ -116,6 +147,17 @@
                 },
                 {
                     data: 'description'
+                },
+                {
+                    data: 'phase',
+                    "render": function(data, type, row) {
+                        if (data === null) {
+                            return '-';
+                        } else {
+                           return data['title'];
+                        }
+                    }
+
                 },
                 {
                     data: null,
@@ -142,6 +184,12 @@
                         '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">' +
                         '<path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>' +
                         '</svg>' +
+                        "</button>" +
+                        "<button class='btn btn-info add_phase'>" +
+                        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-checklist" viewBox="0 0 16 16">' +
+                        '<path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>' +
+                        '<path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>' +
+                        '</svg>' +
                         "</button>"
                 }
 
@@ -155,11 +203,11 @@
 
             $('#create_update').submit(function(event) {
                 event.preventDefault();
-                submit_form(this, clickButtonID,
-                    '{{ route('projects.requirements.store') }}', '#add_requirements', table)
-                    .then(function(res){
-
-                    }).catch(function(res){
+                submit_form(this, clickButtonID, '{{ route('projects.requirements.store') }}',
+                        '#add_requirements', table)
+                    .then(function(res) {
+                        toastr['success'](res.message);
+                    }).catch(function(res) {
                         console.log(res)
                     });
             })
@@ -210,17 +258,17 @@
                 // })
 
 
-                        ajaxfunc('{{ route('projects.requirements.store') }}' + '/' + clickButtonID,
-                    'GET', '').then(function(res){
-                        if(res.message){
+                ajaxfunc('{{ route('projects.requirements.store') }}' + '/' + clickButtonID,
+                        'GET', '').then(function(res) {
+                        if (res.message) {
                             toastr['success'](res.message)
                         }
                         console.log(res)
-                           $('#title').val(res.title);
-                    $('#description').val(res.description);
-                    $('#add_requirements').modal('show');
+                        $('#title').val(res.title);
+                        $('#description').val(res.description);
+                        $('#add_requirements').modal('show');
                     })
-                    .catch(function (res) {
+                    .catch(function(res) {
 
 
                     })
@@ -241,9 +289,22 @@
             })
 
 
+            $(document).on('click', '.add_phase', function(e) {
+                clickButtonID = $(this).attr('data-id');
+                $('#add_phase').modal('show');
+            })
 
-
-
+            $('#add_requrements_phase').submit(function(event) {
+                event.preventDefault();
+                submit_form(this, clickButtonID, "/requirements/" + clickButtonID + "/phases", '#add_phase',
+                        table, false)
+                    .then(function(res) {
+                        toastr['success'](res.message);
+                    }).catch(function(res) {
+                        console.log(res)
+                    });
+                // console.log(clickButtonID)
+            })
         });
     </script>
 @endsection
