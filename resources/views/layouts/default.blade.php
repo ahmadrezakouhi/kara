2128405f5f059dba9d5028b5a88508c77d873e3b
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-06-27 12:03:56 +0430

    chnage location of include navbar in default.blade.php

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,15 +4,16 @@
        @include('includes.head')
     </head>
     <body>
+        @include('includes.header')
     <div class="container">
        <header class="row">
-           @include('includes.header')
+
        </header>
        <div id="main" class="row">
                @yield('content')
        </div>
        <footer class="row">
-           @include('includes.footer') 
+           @include('includes.footer')
            @yield('scripts')
        </footer>
     </div>
