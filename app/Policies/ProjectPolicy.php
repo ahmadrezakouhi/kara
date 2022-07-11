887be410d4d8f5d60e6235d47ddeeeb112495061
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-07-11 11:07:17 +0430

    implement delete() in ProjectPolicy

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -73,6 +73,13 @@ class ProjectPolicy
     public function delete(User $user, Project $project)
     {
 
+        if($project->requirements()->count() != 0){
+            return Response::deny('پروژه دارای نیازمندی می باشد');
+        }
+
+        if($project->phases()->count() != 0){
+            return Response::deny('پروژه دارای فاز می باشد.');
+        }
 
         if(!$user->isAdmin()){
             return Response::deny('امکان حذف پروژه وجود ندارد.');
