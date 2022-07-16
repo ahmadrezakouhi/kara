6569011e2fa968802605dc625efd61f443fff98a
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-07-16 09:40:54 +0430

    implement UserRequest

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -24,7 +24,24 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            
+            'fname' => ['required'],
+            'lname' => ['required'],
+            'email' => ['required'],
+            'mobile' => ['required'],
+            'phone' => ['required'],
+            'role' => ['required'],
+            'password' => ['required']
+        ];
+    }
+
+
+
+    public function messages()
+    {
+        return [
+            'fname.required' => 'فیلد نام الزامی است.',
+            'lname.required' => 'فیلد نام خانوادگی الزامی است.',
+            'email'
         ];
     }
 }
