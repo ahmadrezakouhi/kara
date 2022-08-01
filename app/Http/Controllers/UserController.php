e52499ebf24507403dd3c0cc7f8578c37d59ff03
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-08-01 10:00:04 +0430

    implement UserController methods

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,11 +10,19 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
 
-        if($request->ajax()){
-            $users = User::select('id','fname','lname','mobile',
-            'role','background_color','text_color')->get();
+        if ($request->ajax()) {
+            $users = User::select(
+                'id',
+                'fname',
+                'lname',
+                'mobile',
+                'role',
+                'background_color',
+                'text_color'
+            )->get();
             $recordTotal = count($users);
             $data = array(
                 "draw" => intval($request['draw']),
@@ -26,23 +34,31 @@ class UserController extends Controller
             return response()->json($data);
         }
         return view('user.index');
-
     }
 
-    public function store(UserRequest $request ,User $user = null){
+    public function store(UserRequest $request, $user_id = null)
+    {
 
         $inputs = $request->all();
-        $inputs['password'] = Hash::make($request->password);
-        $inputs['user_id']=Auth::id();
-        unset($inputs['confirm_password']);
-        User::updateOrCreate(['id'=>$user->id],$inputs);
-        return response()->json(['message'=>'کاربر افزوده شد.']);
+        if($user_id){
+            $user = User::find($user_id);
+            $inputs['password']=$user->password;
+        }
+        if ($request->has('changePassword')) {
+            $inputs['password'] = Hash::make($request->password);
+        }
 
+
+        $inputs['user_id'] = Auth::id();
+        unset($inputs['confirm_password']);
+        User::updateOrCreate(['id' => $user_id], $inputs);
+        return response()->json(['message' => 'کاربر افزوده شد.']);
     }
 
 
 
-    public function edit(User $user){
+    public function edit(User $user)
+    {
 
         return response()->json($user);
     }
@@ -50,12 +66,10 @@ class UserController extends Controller
 
 
 
-    public function destroy(User $user){
+    public function destroy(User $user)
+    {
 
         $user->delete();
-        return response()->json(['message'=>'کاربر مورد نظر حذف شد.']);
-
+        return response()->json(['message' => 'کاربر مورد نظر حذف شد.']);
     }
-
-
 }
